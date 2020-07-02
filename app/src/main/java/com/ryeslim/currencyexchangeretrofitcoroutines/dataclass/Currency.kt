d23e9cac8ac0c9518153f8838a920c99865e75a1
Your Name <rutaslv@hotmail.lt>
2020-07-02 22:49:01 +0300

    Initial commit

---
+++ app/src/main/java/com/ryeslim/currencyexchangeretrofitcoroutines/dataclass/Currency.kt
@@ -0,0 +1,13 @@
+package com.ryeslim.currencyexchangeretrofitcoroutines.dataclass
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.math.BigDecimal
+
+data class Currency(
+    @Expose
+    @SerializedName("amount")
+    var balanceValue: BigDecimal,
+    @Expose
+    @SerializedName("currency")
+    var currencyCode: String)
