825267d0be907adc0cb8d7a307ecb344c6d8a896
Your Name <rutaslv@hotmail.lt>
2020-07-22 14:51:48 +0300

    replace the info message String variable with InfoMessage data object.

--- app/src/main/java/com/ryeslim/currencyexchange/MainViewModel.kt
+++ app/src/main/java/com/ryeslim/currencyexchange/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ryeslim.currencyexchange.retrofit.ServiceFactory
 import com.ryeslim.currencyexchange.dataclass.Currency
+import com.ryeslim.currencyexchange.dataclass.InfoMessage
 import kotlinx.coroutines.*
 import java.math.BigDecimal
 
@@ -27,8 +28,8 @@ class MainViewModel : ViewModel() {
     val jpy: LiveData<Currency>
         get() = _jpy
 
-    private val _infoMessage = MutableLiveData<String>()
-    val infoMessage: LiveData<String>
+    private val _infoMessage = MutableLiveData<InfoMessage>()
+    val infoMessage: LiveData<InfoMessage>
         get() = _infoMessage
 
     private val _eurCommission = MutableLiveData<BigDecimal>()
@@ -62,7 +63,7 @@ class MainViewModel : ViewModel() {
         )
         _jpyCommission.value = 0.toBigDecimal()
 
-        _infoMessage.value = ""
+//        _infoMessage.value = ""
     }
 
     val currencies = arrayOf(_eur, _usd, _jpy)
@@ -101,12 +102,12 @@ class MainViewModel : ViewModel() {
                 calculateValues()
                 makeInfoMessage()
             } else {
-                _infoMessage.postValue("Error")
+//                _infoMessage.postValue("Error")
             }
 
         } catch (e: Exception) {
             e.stackTrace
-            _infoMessage.postValue("Error")
+//            _infoMessage.postValue("Error")
         }
     }
 
@@ -145,14 +146,15 @@ class MainViewModel : ViewModel() {
 
     private fun makeInfoMessage() {
         _infoMessage.postValue(
-            String.format(
-                "You converted %.2f %s to %.2f %s. Commission paid: %.2f %s",
-                amountToConvert,
-                currencies[indexFrom].value?.currencyCode,
+//            String.format(
+//                "You converted %.2f %s to %.2f %s. Commission paid: %.2f %s",
+           InfoMessage(
+            amountToConvert,
+                currencies[indexFrom].value?.currencyCode!!,
                 response!!.body()!!.balanceValue,
-                currencies[indexTo].value?.currencyCode,
+                currencies[indexTo].value?.currencyCode!!,
                 thisCommission,
-                currencies[indexFrom].value?.currencyCode
+                currencies[indexFrom].value?.currencyCode!!
             )
         )
     }
