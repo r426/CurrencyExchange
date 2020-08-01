78ec98734d76e1007cc2718210ab1e3ccc455605
Your Name <rutaslv@hotmail.lt>
2020-08-01 09:36:42 +0300

    extract currency code constants into companion object

--- app/src/main/java/com/ryeslim/currencyexchange/utils/initial/InitialBalanceProvider.kt
+++ app/src/main/java/com/ryeslim/currencyexchange/utils/initial/InitialBalanceProvider.kt
@@ -6,21 +6,21 @@ class InitialBalanceProvider {
     fun getInitialEurBalance(): Currency {
         return Currency(
             EUR_BALANCE_INITIAL.toBigDecimal(),
-            "EUR"
+            EUR_CURRENCY_CODE
         )
     }
 
     fun getInitialUsdBalance(): Currency {
         return Currency(
             USD_BALANCE_INITIAL.toBigDecimal(),
-            "USD"
+            USD_CURRENCY_CODE
         )
     }
 
     fun getInitialJpyBalance(): Currency {
         return Currency(
             JPY_BALANCE_INITIAL.toBigDecimal(),
-            "JPY"
+            JPY_CURRENCY_CODE
         )
     }
 
@@ -28,5 +28,8 @@ class InitialBalanceProvider {
         const val EUR_BALANCE_INITIAL = 1000
         const val USD_BALANCE_INITIAL = 0
         const val JPY_BALANCE_INITIAL = 0
+        const val EUR_CURRENCY_CODE = "EUR"
+        const val USD_CURRENCY_CODE = "USD"
+        const val JPY_CURRENCY_CODE = "JPY"
     }
 }
